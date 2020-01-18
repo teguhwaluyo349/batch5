55f4bac887be61c129ed05f1ed9386e1faf83160
DESKTOP-OUVOTGD\adwpc <aceyunus@gmail.com>
2020-01-18 15:59:10 +0700

    soal 2

---
+++ 2.php
@@ -0,0 +1,8 @@
+
+<?php
+
+$data = [['Keren'], ['D', 'W', 'B', 'T', 'A', 'S', 'U', 'D', 'M', 'O', 'Y', 'T', 'I', 'D'], ['Sekali']];
+
+
+echo $data[1][7] . $data[1][6] . $data[1][8] . $data[1][2] . $data[1][1] . $data[1][4] . $data[1][10] . $data[1][5] . $data[1][13] . $data[1][9] . $data[1][11] . $data[1][12] . $data[1][0] . " " . $data[0][0] . " " . $data[2][0];
+?>  
